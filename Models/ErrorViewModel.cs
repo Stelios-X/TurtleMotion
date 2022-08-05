e4c4d17d04b64caa3aab4ff1358f52f1577cf560
Robert Angote <ebobby@outlook.com>
2022-08-05 06:07:44 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pinklazer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
