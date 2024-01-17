1453edc29843d24c5d8495b9a7d9bfe92c97f797
Robert Angote <ebobby@outlook.com>
2024-01-17 19:23:20 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Pinklazer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
